7ceef41d49b5028eeaabe96a7f946359fa0923db
Cesar Roman <cesardroman18@gmail.com>
2023-04-24 18:30:50 -0500

    connected to postgres and created sample connection

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
+import { testConnection } from '../database';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use('/', (req, res) => {
-  res.json({ message: 'hello' });
+app.use('/', (req: Request, res: Response) => {
+  try {
+    testConnection();
+    res.json({ message: 'hello' });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(`listening on localhost:${PORT}`);
 });

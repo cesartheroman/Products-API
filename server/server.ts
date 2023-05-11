5b7a71fc88a83723896ce55024126144e0e4b7e8
Cesar Roman <cesardroman18@gmail.com>
2023-05-11 17:01:08 -0500

    removed sample request

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
@@ -10,14 +10,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.get('/', async (req: Request, res: Response) => {
-  try {
-    res.json({ message: 'hello' });
-  } catch (err) {
-    console.error(err);
-  }
-});
 
 app.use('/products', productRoutes);
 

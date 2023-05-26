5d7a5f64a53015f75631631055411a6dd6785075
Cesar Roman <cesardroman18@gmail.com>
2023-05-26 09:58:50 -0500

    moved queries to definitions folder and added redisQueries to match

--- server/models/definitions/index.ts
+++ server/models/definitions/index.ts
@@ -1,27 +1,2 @@
-export interface Features {
-  feature: string;
-  value: string;
-}
-
-export interface Product {
-  product_id: number;
-  name: string;
-  slogan: string;
-  description: string;
-  category: string;
-  default_price: number;
-  features?: Features[];
-}
-
-export interface NewProduct {
-  name: string;
-  slogan: string;
-  description: string;
-  category: string;
-  default_price: number;
-  features: Features[];
-}
-
-export interface ArrayToJsonObject {
-  array_to_json: number[];
-}
+export { Features, Product, NewProduct, ArrayToJsonObject } from './interfaces';
+export { pgQueries, redisQueries } from './queries';
--- server/models/index.ts
+++ server/models/index.ts
@@ -1,6 +1,12 @@
 import { db, redisClient } from '../../database';
 
-import { Product, ArrayToJsonObject, NewProduct } from './definitions';
+import {
+  Product,
+  ArrayToJsonObject,
+  NewProduct,
+  pgQueries,
+  redisQueries,
+} from './definitions';
 
 //Read All Products
 export const readProductsList = async (
@@ -11,11 +17,14 @@ export const readProductsList = async (
 
   try {
     const query = {
-      text: 'SELECT * FROM products WHERE product_id > $1 ORDER BY product_id LIMIT $2',
+      text: pgQueries.queryProductsList,
       values: [page * count - count, count],
     };
 
-    const redisKey: string = `${page * count - count}`;
+    const redisKey: string = redisQueries.queryProductsList(
+      page * count - count,
+      count
+    );
 
     const cacheValue = await redisClient.get(redisKey);
 
@@ -46,23 +55,11 @@ export const readProductById = async (
 
   try {
     const query = {
-      text: `SELECT *, 
-      (
-        SELECT array_to_json(array_agg(feature_cols)
-      ) 
-      FROM 
-      (
-        SELECT feature, value 
-          FROM features 
-            WHERE features.product_id = $1) feature_cols
-      ) 
-      AS features 
-      FROM products 
-      WHERE products.product_id = $1;`,
+      text: pgQueries.queryProductById,
       values: [product_id],
     };
 
-    const redisKey: string = `${product_id}`;
+    const redisKey: string = redisQueries.queryProductById(product_id);
 
     const cacheValue = await redisClient.get(redisKey);
 
@@ -91,37 +88,11 @@ export const readProductStyles = async (product_id: number) => {
 
   try {
     const query = {
-      text: `SELECT row_to_json(t)
-      FROM (
-        SELECT products.product_id,
-          ( 
-          SELECT array_to_json(array_agg(results_col))
-            FROM (
-              SELECT styles.styles_id, styles.name, styles.original_price, styles.sale_price, styles.default_style,
-              (
-                SELECT array_to_json(array_agg(row_to_json(photo_cols)))
-                FROM (
-                  SELECT thumbnail_url, url 
-                  FROM photos
-                  WHERE photos.styles_id = styles.styles_id
-                ) photo_cols
-              ) AS photos,
-              (
-                SELECT jsonb_object_agg(size, quantity)
-                FROM skus
-                WHERE skus.styles_id = styles.product_id
-              ) AS skus
-            FROM styles
-            WHERE styles.product_id = $1
-          ) results_col
-        ) AS results
-        FROM products
-        WHERE products.product_id = $1
-      ) t;`,
+      text: pgQueries.queryProductStyles,
       values: [product_id],
     };
 
-    const redisKey: string = `${product_id}`;
+    const redisKey: string = redisQueries.queryProductStyles(product_id);
 
     const cachedValue = await redisClient.get(redisKey);
 
@@ -152,11 +123,11 @@ export const readRelatedProoductIds = async (
 
   try {
     const query = {
-      text: `SELECT ARRAY_TO_JSON(ARRAY_AGG(related_product_id)) FROM related WHERE product_id=$1`,
+      text: pgQueries.queryRelatedProductIds,
       values: [product_id],
     };
 
-    const redisKey = `${product_id}`;
+    const redisKey = redisQueries.queryRelatedProductIds(product_id);
 
     const cachedValue = await redisClient.get(redisKey);
 
@@ -187,7 +158,7 @@ export const createNewProduct = async (
 
   try {
     const productQuery = {
-      text: `INSERT INTO products (name, slogan, description, category, default_price) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+      text: pgQueries.queryCreateProduct,
       values: [
         newProduct.name,
         newProduct.slogan,
@@ -204,7 +175,7 @@ export const createNewProduct = async (
     const [featuresObj] = newProduct.features;
 
     const featuresQuery = {
-      text: 'INSERT INTO features (product_id, feature, value) VALUES ($1, $2, $3) RETURNING *',
+      text: pgQueries.queryFeatures,
       values: [
         createdProduct.product_id,
         featuresObj.feature,
@@ -229,7 +200,7 @@ export const updateProductById = async (productToBeUpdated: Product) => {
 
   try {
     const productQuery = {
-      text: 'UPDATE products SET (name, slogan, description, category, default_price) = ($1, $2, $3, $4, $5) WHERE product_id=$6 RETURNING *',
+      text: pgQueries.queryUpdatedProduct,
       values: [
         productToBeUpdated.name,
         productToBeUpdated.slogan,
@@ -259,7 +230,7 @@ export const deleteProductById = async (
 
   try {
     const query = {
-      text: 'DELETE FROM products WHERE product_id=$1 RETURNING *',
+      text: pgQueries.queryDeleteProduct,
       values: [product_id],
     };
 

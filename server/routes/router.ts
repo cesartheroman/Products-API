c2d64acd085e8342324f3ee33ea29eff4ddc4e94
Cesar Roman <cesardroman18@gmail.com>
2023-06-05 13:24:45 -0500

    fixed typo

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -24,8 +24,6 @@ export const productsRouter = Router();
  *  schemas:
  *     Product:
  *      type: object
- *      required:
-        - product_id
         properties:
  *          product_id:
  *              type: integer

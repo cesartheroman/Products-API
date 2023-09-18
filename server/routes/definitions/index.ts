20b6290a63deaa5ea22db1ca1541904b354474e6
Cesar Roman <cesardroman18@gmail.com>
2023-09-18 15:37:05 -0500

    prepping for migration to render

--- server/routes/definitions/index.ts
+++ server/routes/definitions/index.ts
@@ -1,8 +1,5 @@
 import path from 'path';
 export const PORT = process.env.PORT;
-export const SERVER_1 = process.env.SERVER_1;
-export const SERVER_EC2_1 = process.env.SERVER_EC2_1;
-export const SERVER_EC2_2 = process.env.SERVER_EC2_2;
 export const LOCAL_HOST = process.env.LOCAL_HOST;
 
 /* Swagger definition */
@@ -14,15 +11,6 @@ export const swaggerSpec = {
       version: '1.0.0',
     },
     servers: [
-      {
-        url: `${SERVER_1}:${PORT}`,
-      },
-      {
-        url: `${SERVER_EC2_1}:${PORT}`,
-      },
-      {
-        url: `${SERVER_EC2_2}:${PORT}`,
-      },
       {
         url: `${LOCAL_HOST}:${PORT}`,
       },
